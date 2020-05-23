c6c75cd68f2d9223e9ecc3ddec353a10045f4f6f
doyle34 <53892676+doyle34@users.noreply.github.com>
2020-05-23 13:11:05 +0900

    Update main.ino
    - while문 전체 삭제
    - while문 대신 매 loop마다 rudder 각도와 운행 모드(직진, 좌회전, 우회전) 설정
    - timer 사용해서 센서값 읽음
    - BT 조종 전체 삭제
    - BT 이용 값 보내기 기능 추가
    - 함수 선언, 정의 분리
    - yaw angle 정의 방식 변경
    - turnRight, turnLeft를 90도 도는 것에서 매 호출시마다 rudder 각 변경만 하는 것으로 변경
    - goRight, goLeft 대신 setrudderangle 사용
    - angle간의 연산 (addAngle)
    - indent 재정렬 (LLVM)

--- src/main.ino
+++ src/main.ino
@@ -2,15 +2,18 @@
 #include <LiquidCrystal_I2C.h>
 #include <NewPing.h>
 #include <Servo.h>
-#include <SoftwareSerial.h>
 #include <SimpleTimer.h>
+#include <SoftwareSerial.h>
 
 // 1 analog pin for cds
 // 2 digital pin for sonar
 #define BAUD_RATE 115200
 #define BT_BAUD_RATE 9600
 #define SBUF_SIZE 64
-#define Initial_Angle 95
+#define ZERO_RUDDER_ANGLE 95
+#define LEFT_RUDDER_ANGLE_MAX 160
+#define RIGHT_RUDDER_ANGLE_MAX 20
+#define PI 3.14159
 
 int CDS_PIN[3] = {A0, A1, A2};
 int TRIG_PIN[3] = {31, 35, 39};
@@ -22,8 +25,8 @@ int LPWM = 6;
 int RUDDER_PIN = 9;
 int BT_TX_PIN = 50;
 int BT_RX_PIN = 51;
-//int EBI_TX_PIN = 14;
-//int EBI_RX_PIN = 15;
+// int EBI_TX_PIN = 14;
+// int EBI_RX_PIN = 15;
 
 int speed = 35;
 long distance[3] = {200, 200, 200}; // right, center, left
@@ -35,7 +38,7 @@ int brightlim = 400;
 float euler = 0;      // euler is EBimu sensor value. range = (-180, 180)
 float euler_init = 0; // indicates euler angle when ship starts operating
 float yaw = 0;        // yaw is measured w.r.t euler_init. range = (-180, 180)
-float yaw_temp = 0;   // temporal yaw value used when turning
+float yaw_target = 0; // temporal yaw value used when turning
 
 Servo servo;
 SoftwareSerial BTSerial(BT_TX_PIN, BT_RX_PIN);
@@ -43,50 +46,21 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 SimpleTimer timer;
 
-////////////////////////////////////////////////////////////////////
-/////////////////////// EBIMU FUNCTION /////////////////////////////
+int EBimuAsciiParser(float *item, int number_of_item);
+void goForward();
+void goBackward();
+void motorStop();
+void setRudderAngle(int angle);
+void updateSensorValues();
+void setInitEuler();
+float getYaw(float ref_angle);
+float addAngle(float angle1, float angle2);
+void turnRight(float yaw_target);
+void turnLeft(float yaw_target);
+long getDistance(int idx);
+void sendValues(SoftwareSerial s);
 
-int EBimuAsciiParser(float *item, int number_of_item)
-{
-  int n, i;
-  int rbytes;
-  char *addr;
-  int result = 0;
-  char sbuf[SBUF_SIZE];
-  signed int sbuf_cnt = 0;
-
-  rbytes = Serial3.available();
-  for (n = 0; n < rbytes; n++)
-  {
-    sbuf[sbuf_cnt] = Serial3.read();
-    if (sbuf[sbuf_cnt] == 0x0a)
-    {
-      addr = strtok(sbuf, ",");
-      for (i = 0; i < number_of_item; i++)
-      {
-        item[i] = atof(addr);
-        addr = strtok(NULL, ",");
-      }
-
-      result = 1;
-    }
-    else if (sbuf[sbuf_cnt] == '*')
-    {
-      sbuf_cnt = -1;
-    }
-
-    sbuf_cnt++;
-    if (sbuf_cnt >= SBUF_SIZE)
-      sbuf_cnt = 0;
-  }
-
-  return result;
-}
-/////////////////////// EBIMU FUNCTION /////////////////////////////
-////////////////////////////////////////////////////////////////////
-
-void setup()
-{
+void setup() {
   Serial.begin(BAUD_RATE);
   servo.attach(RUDDER_PIN);
   servo.write(91);
@@ -95,9 +69,7 @@ void setup()
   lcd.begin();
   lcd.backlight();
 
-
-  for (int i = 0; i < 3; i++)
-  {
+  for (int i = 0; i < 3; i++) {
     pinMode(TRIG_PIN[i], OUTPUT);
     pinMode(ECHO_PIN[i], INPUT);
   }
@@ -111,7 +83,8 @@ void setup()
 
   // initial yaw angle
   setInitEuler();
-  gofront();
+  yaw_target = getYaw(euler_init);
+  goForward();
   delay(500);
 
   delay(500);
@@ -119,76 +92,85 @@ void setup()
   timer.setInterval(100, updateSensorValues);
 }
 
-void loop()
-{
+void loop() {
   timer.run();
 
-  switch (count)
-  {
+  yaw = getYaw(euler_init);
+
+  // 오른쪽으로 돌아야 하는 상황 가정
+  if (/* nextstepturnright == True*/) {
+    yaw_target = addAngle(yaw, 90); // target yaw를 현재 yaw + 90으로 지정
+    // bool isTurningRight = True;
+  }
+
+  if (/*isTurningRight == True*/) {
+    turnRight(yaw_target); // 오른쪽으로 쬐끔 돈다
+  }
+
+  if (/* nextstepturnleft == True*/) {
+    yaw_target = addAngle(yaw, -90);
+  }
+
+  if (/*isTurningRight == True*/) {
+    turnLeft(yaw_target); // 왼쪽으로 쬐끔 돈다
+  }
+
+  switch (count) {
   case 0:
-    if (distance[1] < distancelim)
-    {
+    if (distance[1] < distancelim) {
       turnLeft();
       count++;
     }
     break;
   case 1:
-    if (distance[0] > 100 || distance[1] < 120)
-    {
+    if (distance[0] > 100 || distance[1] < 120) {
       turnRight();
       count++;
     }
     break;
   case 2:
-    if (distance[0] > 100 || distance[1] < 120)
-    {
+    if (distance[0] > 100 || distance[1] < 120) {
       turnRight(); //대충 우회전 하라는 뜻
       count++;
     }
     break;
   case 3:
-    if (distance[2] > 120 || distance[1] < 120)
-    {
+    if (distance[2] > 120 || distance[1] < 120) {
       turnLeft(); //대충 좌횟=전 하라는뜻
       count++;
     }
     break;
   case 4:
-    if (distance[1] < distancelim)
-    {
+    if (distance[1] < distancelim) {
       turnLeft(); //좌회전
       count++;
     }
     break;
   case 5:
-    if (distance[0] > 100 || distance[1] < 120)
-    {
+    if (distance[0] > 100 || distance[1] < 120) {
       turnRight(); //우회전
       count++;
     }
     break;
   case 6:
-    if (distance[0] > 100 || distance[1] < 120)
-    {
+    if (distance[0] > 100 || distance[1] < 120) {
       turnRight(); //대충 우회전 하라는 뜻
       count++;
     }
     break;
   case 7:
-    if (distance[2] > 120 || distance[1] < 120)
-    {
+    if (distance[2] > 120 || distance[1] < 120) {
       turnLeft();
       count++;
     }
 
     break;
   }
-  gofront();
-  delay(300);
+  goForward();
+  delay(100);
 }
 
-void gofront()
-{
+void goForward() {
   analogWrite(RPWM, speed);
   analogWrite(LPWM, 0);
   servo.attach(RUDDER_PIN);
@@ -197,8 +179,7 @@ void gofront()
   servo.detach();
 }
 
-void gobackward()
-{
+void goBackward() {
   analogWrite(RPWM, 0);
   analogWrite(LPWM, speed);
   servo.attach(RUDDER_PIN);
@@ -207,29 +188,7 @@ void gobackward()
   servo.detach();
 }
 
-void goright(int angle)
-{
-  analogWrite(RPWM, speed);
-  analogWrite(LPWM, 0);
-  if (angle < 20)
-  {
-    angle = 20;
-  }
-  servo.attach(RUDDER_PIN);
-  ;
-  servo.write(angle);
-}
-
-void goleft(int angle)
-{
-  analogWrite(RPWM, speed);
-  analogWrite(LPWM, 0);
-  servo.attach(RUDDER_PIN);
-  servo.write(angle);
-}
-
-void motorstop()
-{
+void motorStop() {
   analogWrite(RPWM, 0);
   analogWrite(LPWM, 0);
   servo.attach(RUDDER_PIN);
@@ -238,12 +197,20 @@ void motorstop()
   servo.detach();
 }
 
-void updateSensorValues()
-{ /*
-  update sensor values (distance, cds, euler)
-  */
-  for (int i = 0; i < 3; i++)
-  {
+void setRudderAngle(int angle) {
+  //
+  int rem = angle % 3;
+  int mod_angle;
+  if (rem == 1) mod_angle = angle - 1;
+  if (rem == 2) mod_angle = angle + 1;
+  servo.attach(RUDDER_PIN);
+  servo.write(mod_angle);
+}
+
+void updateSensorValues() { /*
+                            update sensor values (distance, cds, euler)
+                            */
+  for (int i = 0; i < 3; i++) {
     distance[i] = getDistance(i);
     cds_val[i] = analogRead(CDS_PIN[i]);
   }
@@ -252,90 +219,67 @@ void updateSensorValues()
   euler = euler_temp[2];
 }
 
-void setInitEuler()
-{
+void setInitEuler() {
   float temp = 0;
-  for (int i = 0; i < 10; i++)
-  {
+  for (int i = 0; i < 10; i++) {
     temp += getYaw();
     delay(100);
   }
   euler_init = temp * 0.1; // mean of first 10 values
 }
 
-float getYaw(float ref_angle)
-{ /*
-  calculate yaw angle w.r.t ref_angle
-  if ref_angle == euler_init,
-    return ordinary yaw (relative direction w.r.t initial direction)
-  if ref_angle == yaw_temp,
-    return yaw for rotation (which will be iterated until yaw == -90 or +90)
-  
-  ALWAYS RETURN VALUES BETWEEN (-180, 180)
-  */
+float getYaw(float ref_angle) { /*
+                                calculate yaw angle w.r.t ref_angle
+                                if ref_angle == euler_init,
+                                  return ordinary yaw (relative direction w.r.t
+                                initial direction) if ref_angle == yaw_temp,
+                                  return yaw for rotation (which will be
+                                iterated until yaw == -90 or +90)
+
+                                ALWAYS RETURN VALUES BETWEEN (-180, 180)
+                                */
   yaw = euler - ref_angle;
-  if (yaw > 180) yaw -= 360;
-  if (yaw < -180) yaw += 360;
+  if (yaw > 180)
+    yaw -= 360;
+  if (yaw < -180)
+    yaw += 360;
   return yaw;
 }
 
-float getRemainder(float dividend, float divisor)
+float addAngle(float angle1, float angle2) {
+  float result = angle1 + angle2;
+  if (result > 180)
+    result -= 360;
+  if (result < -180)
+    result += 360;
+  return result;
+}
+
+void turnRight(float yaw_target) // turn until yaw is 90
 {
-  float temp = dividend / divisor;
-  temp = (temp - floor(temp)) * divisor;
-  if (temp > 180)
-  {
-    return temp - 360.0;
-  }
-  else
-  {
-    return temp;
-  }
+  // yaw = getYaw(euler_init); // turn 시작 전 현재 yaw
+  yaw_temp = getYaw(yaw_target); // 목표 yaw 기준 좌표계 재정의
+  delta_yaw = addAngle(yaw_target, -yaw_temp);
+
+  int angle = int(
+    (ZERO_RUDDER_ANGLE - RIGHT_RUDDER_ANGLE_MAX)
+    * cos(delta_yaw * PI / 180)
+    + RIGHT_RUDDER_ANGLE_MAX);
+  setRudderAngle(angle);
 }
 
-void turnRight()
-{ // turn until yaw is 90
-  Serial.println("TurnRight");
-  updateDisp();
-  while (abs(getRemainder((yaw - yaw_temp), 360.0) - 90) > 1)
-  {
-    int angle = int(90 - 90 * cos(((yaw - yaw_temp) * 71) / 4068.0));
-    goright(angle);
-    updateDisp();
-    delay(100);
-  }
-  yaw_temp = yaw;
-  Serial.println("");
+void turnLeft(float yaw_target) {
+  yaw_temp = getYaw(yaw_target); // 목표 yaw 기준 좌표계 재정의
+      delta_yaw = addAngle(yaw_target, -yaw_temp);
+
+    int angle = int(
+      - (LEFT_RUDDER_ANGLE_MAX - ZERO_RUDDER_ANGLE)
+      * cos(delta_yaw * PI / 180)
+      + LEFT_RUDDER_ANGLE_MAX;
+    setRudderAngle(angle);
 }
 
-void turnLeft()
-{ // turn untill yaw is 270
-  Serial.println("TurnLeft");
-  updateDisp();
-  Serial.print("yaw_temp: ");
-  Serial.print(yaw_temp);
-  Serial.print("\t");
-  while (abs(getRemainder((yaw - yaw_temp), 360.0) - (-90)) > 1)
-  {
-    Serial.print("yaw: ");
-    Serial.print(yaw);
-    Serial.print("\t");
-    int angle = int(90 + 90 * cos(((yaw - yaw_temp) * 71) / 4068.0));
-    Serial.print("cos(yaw): ");
-    Serial.print(cos(((yaw - yaw_temp) * 71) / 4068.0));
-    Serial.print("\t");
-    Serial.print("angle: ");
-    Serial.print(angle);
-    Serial.println("");
-    goleft(angle);
-    updateDisp();
-    delay(100);
-  }
-  yaw_temp = yaw;
-}
-
-long getDistance(int idx)
-{
+long getDistance(int idx) {
   digitalWrite(TRIG_PIN[idx], LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN[idx], HIGH);
@@ -364,4 +308,65 @@ void sendValues(SoftwareSerial s) {
   s.print(", ");
   s.print(count);
   s.println("");
+}
+
+////////////////////////////////////////////////////////////////////
+/////////////////////// EBIMU FUNCTION /////////////////////////////
+
+int EBimuAsciiParser(float *item, int number_of_item) {
+  int n, i;
+  int rbytes;
+  char *addr;
+  int result = 0;
+  char sbuf[SBUF_SIZE];
+  signed int sbuf_cnt = 0;
+
+  rbytes = Serial3.available();
+  for (n = 0; n < rbytes; n++) {
+    sbuf[sbuf_cnt] = Serial3.read();
+    if (sbuf[sbuf_cnt] == 0x0a) {
+      addr = strtok(sbuf, ",");
+      for (i = 0; i < number_of_item; i++) {
+        item[i] = atof(addr);
+        addr = strtok(NULL, ",");
+      }
+
+      result = 1;
+    } else if (sbuf[sbuf_cnt] == '*') {
+      sbuf_cnt = -1;
+    }
+
+    sbuf_cnt++;
+    if (sbuf_cnt >= SBUF_SIZE)
+      sbuf_cnt = 0;
+  }
+
+  return result;
+}
+/////////////////////// EBIMU FUNCTION /////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+// legacy functions
+float getRemainder(float dividend, float divisor) {
+  float temp = dividend / divisor;
+  temp = (temp - floor(temp)) * divisor;
+  if (temp > 180) {
+    return temp - 360.0;
+  } else {
+    return temp;
+  }
+}
+
+void goRight(int angle) {
+  analogWrite(RPWM, speed);
+  analogWrite(LPWM, 0);
+  servo.attach(RUDDER_PIN);
+  servo.write(angle);
+}
+
+void goLeft(int angle) {
+  analogWrite(RPWM, speed);
+  analogWrite(LPWM, 0);
+  servo.attach(RUDDER_PIN);
+  servo.write(angle);
 }
